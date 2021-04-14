10cf86b3d5539e12d6ee3499ec16bbe64072248b
sollimann <kristoffer.solberg@cognite.com>
2021-04-14 18:00:54 +0200

    upload trained models

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,5 +1,7 @@
 import logging
+from typing import Tuple, List
 
+import coloredlogs
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
@@ -21,21 +23,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S%z")
 logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO', logger=logger)
 
 
-def main():
-    INIT_LR = 1e-4
-    EPOCHS = 20
-    BS = 32
+def load_data() -> Tuple[List, List]:
     DIRECTORY = rf"{os.path.dirname(__file__)}/dataset"
     CATEGORIES = ["with_mask", "without_mask"]
-
     logger.info(f"loading images from dir: {DIRECTORY}")
-
     data, labels = [], []
-
     for category in CATEGORIES:
         path = os.path.join(DIRECTORY, category)
         for img in os.listdir(path):
@@ -47,7 +43,105 @@ def main():
             data.append(image)
             labels.append(category)
 
-    print(data)
+    logger.info(f"Succesful loading of dataset \n "
+                f"data size: {data.__len__()} \n "
+                f"labels size: {labels.__len__()} \n")
+
+    return data, labels
+
+
+def main():
+    INIT_LR = 1e-4
+    EPOCHS = 20
+    BS = 32
+
+    data, labels = load_data()
+
+    # perform one-hot encoding on the labels
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
+    labels = to_categorical(labels)
+    data = np.array(data, dtype="float32")
+    labels = np.array(labels)
+
+    # split train and test data
+    (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
+
+    # construct the training image generator for data augmentation
+    aug = ImageDataGenerator(
+        rotation_range=20,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest")
+
+    # load the MobileNetV2 network, ensuring the head FC layer sets are left off
+    baseModel = MobileNetV2(weights="imagenet", include_top=False,
+                            input_tensor=Input(shape=(224, 224, 3)))
+
+    # construct the head of the model that will be placed on top of the base model
+    headModel = baseModel.output
+    headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+    headModel = Flatten(name="flatten")(headModel)
+    headModel = Dense(128, activation="relu")(headModel)
+    headModel = Dropout(0.5)(headModel)
+    headModel = Dense(2, activation="softmax")(headModel)
+
+    # place the head FC model on top of the base model (this will become the actual model we will train)
+    model = Model(inputs=baseModel.input, outputs=headModel)
+
+    # loop over all layers in the base model and freeze them so they will
+    # *not* be updated during the first training process
+    for layer in baseModel.layers:
+        layer.trainable = False
+
+    # compile our model
+    logger.info("[INFO] compiling model...")
+    opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+    model.compile(loss="binary_crossentropy", optimizer=opt,
+                  metrics=["accuracy"])
+
+    # train the head of the network
+    logger.info("[INFO] training head...")
+    H = model.fit(
+        aug.flow(trainX, trainY, batch_size=BS),
+        steps_per_epoch=len(trainX) // BS,
+        validation_data=(testX, testY),
+        validation_steps=len(testX) // BS,
+        epochs=EPOCHS)
+
+    # make predictions on the testing set
+    logger.info("[INFO] evaluating network...")
+    predIdxs = model.predict(testX, batch_size=BS)
+
+    # for each image in the testing set we need to find the index of the
+    # label with corresponding largest predicted probability
+    predIdxs = np.argmax(predIdxs, axis=1)
+
+    # show a nicely formatted classification report
+    logger.info(classification_report(testY.argmax(axis=1), predIdxs,
+                                      target_names=lb.classes_))
+
+    # serialize the model to disk
+    logger.info("[INFO] saving mask detector model...")
+    model.save("mask_detector.model", save_format="h5")
+
+    # plot the training loss and accuracy
+    N = EPOCHS
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+    #plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+    plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig("plot.png")
+
 
 if __name__ == "__main__":
     main()
