603f0962b7d7ddfb2dded69cfcc807c2bee5fbaf
sollimann <kristoffer.solberg@cognite.com>
2021-04-15 21:35:53 +0200

    remove un-used files

--- facemask_detector/detect_mask_video.py
+++ facemask_detector/detect_mask_video.py
@@ -84,8 +84,10 @@ def pose_detection(image, poseNet, threshold=0.2):
                   ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
                   ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
 
-    LEG_PARTS = {"RKnee": 9, "RAnkle": 10, "LKnee": 12, "LAnkle": 13}
-    LEG_POSE_PAIRS = [["RKnee", "RAnkle"], ["LKnee", "LAnkle"]]
+    POSE_PAIRS_NO_HEAD = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+                  ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+                  ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+                  ["LHip", "LKnee"], ["LKnee", "LAnkle"]]
 
     (h, w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(
@@ -119,14 +121,14 @@ def pose_detection(image, poseNet, threshold=0.2):
         # Add a point if it's confidence is higher than threshold.
         points.append((int(x), int(y)) if conf > threshold else None)
 
-    for pair in LEG_POSE_PAIRS:
+    for pair in POSE_PAIRS_NO_HEAD:
         partFrom = pair[0]
         partTo = pair[1]
         assert (partFrom in BODY_PARTS)
         assert (partTo in BODY_PARTS)
 
-        idFrom = LEG_PARTS[partFrom]
-        idTo = LEG_PARTS[partTo]
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
 
         if points[idFrom] and points[idTo]:
             cv2.line(image, points[idFrom], points[idTo], (0, 255, 0), 3)
